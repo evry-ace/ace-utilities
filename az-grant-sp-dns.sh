e5261b912e458abbe5325677ee97543d673259d6
Endre Karlson <endre.karlson@gmail.com>
2019-04-11 14:33:53 +0200

    Fix SP_ID queries

--- az-grant-sp-dns.sh
+++ az-grant-sp-dns.sh
@@ -2,7 +2,7 @@ AZ_SP_NAME=$1
 AZ_DNS_RG=$2
 AZ_DNS_NAME=$3
 
-SP_ID=$(az ad app list | jq -r --arg name $AZ_SP_NAME '.[] | select(.displayName == $name) | .appId')
+SP_ID=`az ad app list | jq -r --arg NAME "$AZ_SP_NAME" '.[] | select(.displayName == $NAME) | .appId'`
 
 # Give Access to DNS Zone
 DNS_ID=$(az network dns zone show --name $AZ_DNS_NAME --resource-group $AZ_DNS_RG --query "id" --output tsv)
--- az-grant-sp-waf.sh
+++ az-grant-sp-waf.sh
@@ -2,7 +2,7 @@ AZ_SP_NAME=$1
 AZ_WAF_RG=$2
 AZ_WAF_NAME=$3
 
-SP_ID=$(az ad app list | jq -r --arg name $AZ_SP_NAME '.[] | select(.displayName == $name) | .appId')
+SP_ID=`az ad app list | jq -r --arg NAME "$AZ_SP_NAME" '.[] | select(.displayName == $NAME) | .appId'`
 
 # Give Access to DNS Zone
 WAF_ID=$(az network application-gateway show --name $AZ_WAF_NAME --resource-group $AZ_WAF_RG --query "id" --output tsv)
